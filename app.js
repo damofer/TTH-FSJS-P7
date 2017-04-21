6fdaf3e46dd0a11da42451e68ad8684052f31c5b
David Mogollon <dmogollon@uninorte.edu.co>
2017-04-21 15:01:01 -0500

    Added some functionalities, tweets and friends

--- app.js
+++ app.js
@@ -1,17 +1,11 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+var twitter_credentials = require(__dirname+'/config.js');
 
-var Twit = require('twit');
-var twitter_credentials = require('./config.js');
-var T = new Twit(twitter_credentials);
+// var index = require(__dirname+'/routes/index');
 
+var twit = require(__dirname + '/twit.js');
 
 var app = express();
 
@@ -21,24 +15,69 @@ app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+// app.use('/', index);
+
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+/*app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
+});*/
+
+
+// Middleware - Twitter authentication
+app.use(function(req, res, next) {
+	var user =  twit.getProfile(twitter_credentials);
+	req.user= user;
+	req.latestTweets =twit.getLatestTweets(user);
+	req.recentFriends =twit.getRecentFriends(user);
+	req.latestMessages =twit.getLatestMessages(user);
+
+	
+
+		
+	next();
+});
+
+app.get('/', function(req, res) {
+	var user = req.user;
+	var latestTweets;
+	req.latestTweets.then(function(data){
+		latestTweets =data.data;
+	});
+	var recentFriends;
+	req.recentFriends.then(function(data){
+		recentFriends=data.data.users;
+	});
+
+	var latestMessages;
+	req.latestMessages.then(function(data){
+		latestMessages=data.data;
+	});
+
+	Promise.all([req.latestTweets,req.recentFriends,req.latestMessages]).then(function(){
+		console.dir(user);
+		// console.dir(latestTweets);
+		// console.dir(recentFriends);
+		// console.dir("latestMessages : " + latestMessages);
+		res.render(path.join(__dirname, 'views/index.pug'), { user:user,
+															latestTweets:latestTweets,
+															recentFriends:recentFriends,
+															latestMessages:latestMessages
+															});
+	});
+
+
+  
+
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+/*app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -46,12 +85,12 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
-});
+});*/
 app.listen(3000, function() {
 
 	console.log("The frontend server is running on port 3000!");
 
 
-	
+
 });
 module.exports = app;
---
+++ twit.js
@@ -0,0 +1,48 @@
+var Twit = require('twit');
+
+
+
+// Twit user Auth with Dev Credentials
+function getProfile(credentials) {
+    return new Twit(credentials);
+}
+
+// Using Twitter's API to get the results in a JSON Format
+
+
+
+
+/*API Reference: Returns a collection of the most recent Tweets posted by the user indicated 
+by the screen_name or user_id parameters
+https://dev.twitter.com/rest/reference/get/statuses/user_timeline
+*/
+function getLatestTweets(profile) {
+    return profile.get('statuses/user_timeline', { screen_name: profile.screen_name, count: 5 });
+}
+
+/*API Reference : Returns a cursored collection of user objects for every user the specified user 
+is following (otherwise known as their “friends”).
+At this time, results are ordered with the most recent following first
+https://dev.twitter.com/rest/reference/get/friends/list
+*/
+function getRecentFriends(profile) {
+    return profile.get('friends/list', { screen_name: profile.screen_name , count: 5 });
+}
+
+/*API Reference :Returns the 20 most recent direct messages sent by the authenticating user. 
+Includes detailed information about the sender and recipient user
+https://dev.twitter.com/rest/reference/get/direct_messages/sent
+*/
+function getLatestMessages(profile) {
+    return profile.get('direct_messages/sent', { screen_name: profile.screen_name, count: 5 });
+}
+
+//Object literal module exports
+module.exports = {
+
+	getProfile: getProfile,
+	getLatestMessages: getLatestMessages,
+	getLatestTweets: getLatestTweets,
+	getRecentFriends: getRecentFriends
+}
+
